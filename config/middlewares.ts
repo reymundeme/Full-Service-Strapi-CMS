d89407031c6d6151eb049106d24802d678f16d4a
reymundeme <station@autogenie.ai>
2025-09-27 18:59:36 +0800

    change the middlewares

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,25 @@
-export default [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          'default-src': ["'self'"],
+          'script-src': ["'self'", 'https:', "'unsafe-inline'"],
+          'style-src': ["'self'", 'https:', "'unsafe-inline'"],
+          'font-src': ["'self'", 'https:', 'data:'],
+          'frame-src': ["'self'", 'http://104.248.127.3', 'https://precious-dinosaur-99506b8ce1.strapiapp.com'], // Add your domain and IP here
+          'upgradeInsecureRequests': null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
